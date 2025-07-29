cb49b19feb472c02c18d57a42c369ac3b6dc280a
Jihyeon-Kweon <jiniejinie001125@gmail.com>
2025-07-29 13:29:21 -0600

    Stage 2: Multiple fiels, include dirs, compiler flags

---
+++ build_flow/cmake_stage2/build/CMakeFiles/app.dir/src/main.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/app.dir/src/main.c.o: \
+ /home/jinie/Desktop/jinie/embedded-systems-practice/build_flow/cmake_stage2/src/main.c \
+ /usr/include/stdc-predef.h \
+ /home/jinie/Desktop/jinie/embedded-systems-practice/build_flow/cmake_stage2/include/helper.h
