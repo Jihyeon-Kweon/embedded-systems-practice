c89bcbb8b11e03390b429cd1d8cdfc7d7f2d9bab
Jihyeon-Kweon <jiniejinie001125@gmail.com>
2025-07-30 16:39:38 -0600

    cmake_stage3

---
+++ build_flow/cmake_stage3/build/CMakeFiles/mathlib.dir/libmath/add.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/mathlib.dir/libmath/add.c.o: \
+ /home/jinie/Desktop/jinie/embedded-systems-practice/build_flow/cmake_stage3/libmath/add.c \
+ /usr/include/stdc-predef.h \
+ /home/jinie/Desktop/jinie/embedded-systems-practice/build_flow/cmake_stage3/libmath/add.h
